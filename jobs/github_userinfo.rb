6463a021784809f94b15f80b8983ee1760703cbd
Per Sandström <per@helloper.com>
2013-03-28 16:46:51 +0100

    Make it Kollegorna shiny

---
+++ jobs/github_userinfo.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'json'
+
+# This job will track metrics of a github organisation or user
+
+# Config
+# ------
+# example for tracking single user repositories
+github_username = 'orgs/kollegorna'
+# example for tracking an organisations repositories
+# github_username = 'orgs/foobugs'
+
+SCHEDULER.every '3m', :first_in => 0 do |job|
+  http = Net::HTTP.new("api.github.com", Net::HTTP.https_default_port())
+  http.use_ssl = true
+  response = http.request(Net::HTTP::Get.new("/#{github_username}"))
+  data = JSON.parse(response.body)
+
+  if response.code != "200"
+    puts "github api error (status-code: #{response.code})\n#{response.body}"
+  else
+    send_event('github_userinfo_followers', current: data['followers'])
+    send_event('github_userinfo_following', current: data['following'])
+    send_event('github_userinfo_repos', current: data['public_repos'])
+    send_event('github_userinfo_gists', current: data['public_gists'])
+  end
+end
---
+++ jobs/twitter_user.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'net/http'
+
+# Track public available information of a twitter user like follower, follower
+# and tweet count by scraping the user profile page.
+
+# Config
+# ------
+twitter_username = 'kollegorna'
+
+SCHEDULER.every '2m', :first_in => 0 do |job|
+  http = Net::HTTP.new("twitter.com", Net::HTTP.https_default_port())
+  http.use_ssl = true
+  response = http.request(Net::HTTP::Get.new("/#{twitter_username}"))
+  if response.code != "200"
+    puts "twitter communication error (status-code: #{response.code})\n#{response.body}"
+  else
+
+    tweets = /profile["']>[\n\t\s]*<strong>([\d.]+)/.match(response.body)[1].delete('.').to_i
+    following = /following["']>[\n\t\s]*<strong>([\d.]+)/.match(response.body)[1].delete('.').to_i
+    followers = /followers["']>[\n\t\s]*<strong>([\d.]+)/.match(response.body)[1].delete('.').to_i
+
+    send_event('twitter_user_tweets', current: tweets)
+    send_event('twitter_user_followers', current: followers)
+    send_event('twitter_user_following', current: following)
+  end
+end
